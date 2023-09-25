c584d96afcad37725eed69696ca5a8b7f423e3ef
mauri <mauricio.macedo.villarnobo@gmail.com>
2023-09-25 13:24:49 -0300

    defini-device-para-gpu-no-gerador-do-modelo

--- AI/Swift-SRGAN-RMK/optimize-graph.py
+++ AI/Swift-SRGAN-RMK/optimize-graph.py
@@ -5,9 +5,10 @@ from models import Generator
 import argparse
 
 
+
 def trace(model):
-    x = torch.rand(1, 3, 96, 96)
-    traced_model = torch.jit.trace(model, (x))
+    x = torch.rand(1, 3, 96, 96).to("cuda")
+    traced_model = torch.jit.trace(model, (x)).to("cuda")
     return traced_model
 
 def main(opt):
